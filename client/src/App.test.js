6a21a8b8af0bc1cf83db6d8427895284bedf005d
Krishan Nattar <krishan.nattar@gmail.com>
2019-09-06 08:58:12 -0700

    started testing

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './App';import "@testing-library/jest-dom/extend-expect";
+import * as rtl from "@testing-library/react";
+
+// afterEach(rtl.cleanup)
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
