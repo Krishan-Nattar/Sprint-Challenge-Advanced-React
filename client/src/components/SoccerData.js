8423ca461cebca84b6cb474dc5edd4ed75c5ea24
Krishan Nattar <krishan.nattar@gmail.com>
2019-09-06 08:55:24 -0700

    refactoring

--- client/src/components/SoccerData.js
+++ client/src/components/SoccerData.js
@@ -1,30 +1,23 @@
 import React from "react";
 import useLocalStorage from "../hooks/useLocalStorage";
-import {Card} from 'semantic-ui-react';
+import { Card } from "semantic-ui-react";
 
 const SoccerData = props => {
   const { data } = props;
   const [value, setValue] = useLocalStorage("players", data);
-  console.log(value);
 
   return (
     <Card.Group>
       {value.map(person => {
-        return  <Card key={person.id}>
-        <Card.Content>
-          <Card.Header>{person.name}</Card.Header>
-          <Card.Meta>Searches: {person.searches}</Card.Meta>
-          <Card.Description>
-          {person.country}
-          </Card.Description>
-        </Card.Content>
-      </Card>
-        
-        // <div >
-        //     <p></p>
-        //     <p></p>
-        //     <p></p>
-        //     </div>;
+        return (
+          <Card key={person.id}>
+            <Card.Content>
+              <Card.Header>{person.name}</Card.Header>
+              <Card.Meta>Searches: {person.searches}</Card.Meta>
+              <Card.Description>{person.country}</Card.Description>
+            </Card.Content>
+          </Card>
+        );
       })}
     </Card.Group>
   );
