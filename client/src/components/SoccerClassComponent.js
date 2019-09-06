8d70cccb2e0cc0f7d30c06900692ca92125231c1
Krishan Nattar <krishan.nattar@gmail.com>
2019-09-06 08:19:03 -0700

    class component set up

---
+++ client/src/components/SoccerClassComponent.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import axios from 'axios';
+
+
+class SoccerClassComponent extends React.Component {
+    state = {  }
+    render() { 
+        return ( 
+        
+        <div>
+
+            soccer
+        </div> 
+        );
+    }
+}
+ 
+export default SoccerClassComponent;
