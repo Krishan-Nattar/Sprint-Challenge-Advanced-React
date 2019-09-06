d01105919e3c8dc708d257f6d891e3eb4410dbc1
Krishan Nattar <krishan.nattar@gmail.com>
2019-09-06 08:37:21 -0700

    useLocalStorage working fine

--- client/src/components/SoccerData.js
+++ client/src/components/SoccerData.js
@@ -1,25 +1,22 @@
-import React from 'react';
+import React from "react";
 import useLocalStorage from "../hooks/useLocalStorage";
 
+const SoccerData = props => {
+  const { data } = props;
+  const [value, setValue] = useLocalStorage("players", data);
+  console.log(value);
 
+  return (
+    <div>
+      {value.map(person => {
+        return <div key={person.id}>
+            <p>{person.name}</p>
+            <p>{person.country}</p>
+            <p>Searches: {person.searches}</p>
+            </div>;
+      })}
+    </div>
+  );
+};
 
-
-const SoccerData = (props) => {
-
-    
-    const {data} = props;
-    const [value, setValue] = useLocalStorage('players', data);
-
-    return ( <div>
-        
-        
-        {value.map(person=>{
-            return <p key={person.id}>{person.name}</p>
-        })}
-        
-        
-        
-        </div> );
-}
- 
 export default SoccerData;
