24b759b1be447bfbaf9c13801f7dbd6b120c8f86
Krishan Nattar <krishan.nattar@gmail.com>
2019-09-06 08:22:09 -0700

    axios call bringing up players

--- client/src/components/SoccerClassComponent.js
+++ client/src/components/SoccerClassComponent.js
@@ -4,7 +4,23 @@ import axios from 'axios';
 
 class SoccerClassComponent extends React.Component {
     state = {  }
+
+
+
+    componentDidMount(){
+            axios
+            .get('http://localhost:5000/api/players')
+            .then(res=>{
+                console.log(res.data);
+            })
+    }
+
+
+
     render() { 
+
+
+        
         return ( 
         
         <div>
